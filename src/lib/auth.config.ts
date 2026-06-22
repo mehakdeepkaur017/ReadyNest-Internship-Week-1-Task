11fbe1e38513a99cc7b899815a63c252a4cf327e
mehak <mehakdeepkaur017@gmail.com>
2026-06-22 19:44:12 +0530

    fix: Map name and email to NextAuth session object for Credentials provider

--- src/lib/auth.config.ts
+++ src/lib/auth.config.ts
@@ -9,12 +9,18 @@ export const authConfig = {
     async jwt({ token, user }) {
       if (user) {
         token.id = user.id;
+        token.name = user.name;
+        token.email = user.email;
+        token.picture = user.image;
       }
       return token;
     },
     async session({ session, token }) {
       if (session.user) {
         session.user.id = token.id as string;
+        session.user.name = token.name as string;
+        session.user.email = token.email as string;
+        session.user.image = token.picture as string | undefined;
       }
       return session;
     },
